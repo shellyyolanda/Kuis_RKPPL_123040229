b96bc599a6c425a7a024ac1ea49a1b0c81ec6b37
shelly <shelly.123040229@mail.unpas.ac.id>
2015-04-20 12:16:15 +0700

    Terdapat perubahahan penamaan variabel

---
+++ src/main/java/org/ifunpas/rkppl/kuis_rkppl_123040229/Soal.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ifunpas.rkppl.kuis_rkppl_123040229;
+
+/**
+ *
+ * @author SB 604-04
+ */
+public class Soal {
+    public int pangkat(int bilangan, int pangkat){
+        int hasil=0;
+        int n=1;
+        while(n <= pangkat){
+            n++;
+            hasil *= bilangan;
+        }
+        return hasil;
+    }
+    
+    public int faktorial(int bil) {
+        int hasil = 1;
+        for (int i = 0; i <= bil; i++) {
+            hasil *= i;
+        }
+        return hasil;
+    }
+    
+    public int perkalian(int bilA, int bilB){
+        return bilA * bilB;
+    }
+    
+    public static void main(String[] args) {
+        Soal A = new Soal();
+        A.pangkat(5, 2);
+        A.faktorial(8);
+        A.perkalian(7, 5);
+    }
+}
