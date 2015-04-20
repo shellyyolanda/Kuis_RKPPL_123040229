46b21bdd36bff887d5ff33ffa4e5aa10d5cbd468
shelly <shelly.123040229@mail.unpas.ac.id>
2015-04-20 12:23:04 +0700

    Perubahan angka yang akan dihitung

--- src/main/java/org/ifunpas/rkppl/kuis_rkppl_123040229/Soal.java
+++ src/main/java/org/ifunpas/rkppl/kuis_rkppl_123040229/Soal.java
@@ -33,8 +33,8 @@ public class Soal {
     
     public static void main(String[] args) {
         Soal A = new Soal();
-        A.pangkat(5, 2);
-        A.faktorial(8);
-        A.perkalian(7, 5);
+        A.pangkat(10, 2);
+        A.faktorial(9);
+        A.perkalian(2, 5);
     }
 }
